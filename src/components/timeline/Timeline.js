fb80a9dd97b01328220eeb96b5436ecd1a8d2d58
KremoWy <alanwff4@gmail.com>
2021-02-13 19:33:13 -0300

    desktop timeline

--- src/components/timeline/Timeline.js
+++ src/components/timeline/Timeline.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Moment from 'moment';
 import shortid from 'shortid';
 import SimpleLoadScreen from '../loader/SimpleLoadScreen';
+import csgoLogoDefaultBlack from '../../ImagenesVarias/csgoLogoDefaultBlack.png';
+import toBeDefined from '../../ImagenesVarias/toBeDefined.png';
 import { Link } from 'react-router-dom';
 import {momentSpanishSetup} from '../../utility/MomentSpanishSetup';
 import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
@@ -12,38 +14,49 @@ import './timeline.css'
 const Timeline = () => {
     momentSpanishSetup();
     const styles = getStyles();
-    console.log(data);
     if (styles !== undefined) {
         return ( 
-            <div className="time-line-container" style={{backgroundColor: styles.background_color}}>
+            <div className="timeline-background time-line-container font-gilroy" style={{backgroundColor: styles.background_color}}>
                 <VerticalTimeline>
                     {   
                         data.map((tournament) => {
-                            const {begin_at, league, serie, name, teams, prizepool} = tournament;
+                            const {begin_at, league, serie, name, teams} = tournament;
                             return(
                                 <VerticalTimelineElement
                                     key={shortid.generate()}
                                     className="vertical-timeline-element--education"
-                                    date={Moment(begin_at).format("YYYY-MM-DD    hh:mm")}
-                                    iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
-                                    icon={league.image_url}
+                                    date={Moment(begin_at).format("DD - MMMM - hh:mm")}
+                                    iconStyle={{border: `4px solid ${styles.header_color}`}}
+                                    icon={<Link to={`/${league.name}`} title={`Click para Ver los Próximos partidos de la ${league.name}`}><img className="tournament-logo-timeline" src={league.image_url}/></Link>}
                                     >
                                     <h3 className="vertical-timeline-element-title">{league.name}</h3>
-                                    <h4 className="vertical-timeline-element-subtitle">{serie.full_name}</h4>
-                                    <h5 className="vertical-timeline-element-subtitle">{name}</h5>
-                                    <p>Tier: {serie.tier}</p>
+                                    <h5 className="vertical-timeline-element-subtitle">{serie.full_name}</h5>
+                                    <div className="column-align mb-5px">
+                                        <span className="vertical-timeline-element-subtitle name-of-tournament font-size-20px">{name}</span>
+                                        <span className="font-size-20px">Tier: <span className="font-gilroy-bold">{serie.tier}</span></span>
+                                    </div>
+                                    
                                     <div className="teams-in-tournament">
-                                        {
+                                        {teams.length > 1?
                                             teams.map((team) => (
-                                                <Link to={`/${team.slug}`} key={shortid.generate()}>
+                                                <Link to={`/${team.slug}`} title={`Click para Ver el Perfil de ${team.name}`} key={shortid.generate()}>
                                                     <div className="icon-container">
                                                         <div className="team-icon">
-                                                            <img className="team-logo-timeline" title={team.name} src={team.image_url}></img>
+                                                            <img className="team-logo-timeline" src={team.image_url === null?  csgoLogoDefaultBlack : team.image_url}></img>
                                                         </div>
                                                     </div>
                                                     
                                                 </Link>
                                             ))
+                                            :
+                                            
+                                            <div className="no-team-container">
+                                                <div className="no-team"><img className="team-logo-timeline" src={toBeDefined}/></div>
+                                                <div className="no-team"><img className="team-logo-timeline" src={toBeDefined}/></div>
+                                                <div className="no-team"><img className="team-logo-timeline" src={toBeDefined}/></div>
+                                            </div>
+                                            
+                                            
                                         }
                                     </div>
                                 </VerticalTimelineElement>
@@ -56,7 +69,7 @@ const Timeline = () => {
         );
     }else{
         return(
-            <div className="time-line-container" style={{backgroundColor: 'black'}}>
+            <div className="timeline-background time-line-container" style={{backgroundColor: 'black'}}>
                 <SimpleLoadScreen/>
             </div>
         );
