201f919f025a40216e4db006f1d8a7b6ffbfd012
KremoWy <alanwff4@gmail.com>
2021-03-30 16:02:18 -0300

    planning

--- src/components/tournaments/Tournaments.js
+++ src/components/tournaments/Tournaments.js
@@ -6,7 +6,7 @@ import { Link } from 'react-router-dom';
 import { TournamentContext } from '../context/TournamentContext';
 import {
     TOURNAMENT,
-  } from './routes/routes';
+  } from '../../routes/routes';
 /* import {
     UNITY
 } from '../../routes/routes'; */
