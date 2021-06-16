9e62a0f539fa15b199640d343a9574d331a73ba0
KremoWy <alanwff4@gmail.com>
2021-06-16 16:47:56 -0300

    send email

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,12 +1,22 @@
 import React from "react";
+import { SERVICE_ID, USER_ID, TEMPLATE_ID } from "../../routes/emailkey";
+import { init, send } from "emailjs-com";
 import "./Contact.css";
+init(USER_ID);
+
 const Contact = () => {
     const getInfo = () => {
         const name = document.getElementById("name").value;
         const email = document.getElementById("email").value;
         const message = document.getElementById("message").value;
-        console.log(name, email, message);
-    }
+        const subject = document.getElementById("subject").value;
+        send(SERVICE_ID, TEMPLATE_ID, {
+            subject: subject,
+            from_name: name,
+            email: email,
+            message: message,
+        });
+    };
     return (
         <div className="background-color-4all contact-container">
             <h1 className="font-gilroy-bold">Contact</h1>
@@ -18,13 +28,23 @@ const Contact = () => {
                     </div>
                     <div>
                         <h3 className="font-gilroy">Email</h3>
-                        <input id="email" type="text" placeholder="" aria-label="name" aria-describedby="name-input" />
+                        <input
+                            id="email"
+                            type="text"
+                            placeholder=""
+                            aria-label="email"
+                            aria-describedby="email-input"
+                        />
                     </div>
                 </div>
+                <h3 className="font-gilroy">Subject</h3>
+                <input className="mb-60px" id="subject" type="text" placeholder="" aria-label="subject" aria-describedby="subject-input" />
                 <h3 className="font-gilroy">Message</h3>
                 <textarea id="message"></textarea>
             </div>
-            <div onClick={() => getInfo()} className="send-btn">Send</div>
+            <div onClick={() => getInfo()} className="send-btn">
+                Send
+            </div>
         </div>
     );
 };
---
+++ src/routes/emailkey.js
@@ -0,0 +1,3 @@
+export const SERVICE_ID = process.env.REACT_APP_SERVICE_ID;
+export const TEMPLATE_ID = process.env.REACT_APP_TEMPLATE_ID;
+export const USER_ID = process.env.REACT_APP_USER_ID;
